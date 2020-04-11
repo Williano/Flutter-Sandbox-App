48437ac18a8bd2e5d0eddde9dbfccd98dd788444
Williano <paawilly17@gmail.com>
2020-04-10 22:21:13 -0500

    Add new practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sandbox/screens/nested_bottom_navigation_bar_screen.dart';
 import 'package:flutter_sandbox/screens/sliver_screen.dart';
 
 void main() => runApp(SandboxApp());
@@ -12,7 +13,7 @@ class SandboxApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SliverScreen(),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/screens/nested_bottom_navigation_bar_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sandbox/screens/sliver_screen.dart';
+
+class NavigatorPage extends StatefulWidget {
+  const NavigatorPage({Key key, this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  _NavigatorPageState createState() => _NavigatorPageState();
+}
+
+class _NavigatorPageState extends State<NavigatorPage> {
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController(
+      text: 'sample text: ${widget.child}',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      onGenerateRoute: (RouteSettings settings) {
+        return new MaterialPageRoute(
+          settings: settings,
+          builder: (BuildContext context) {
+            return Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  widget.child,
+                  SizedBox(height: 16.0),
+                  RaisedButton(
+                    child: Text('push a route'),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return Scaffold(
+                            appBar: AppBar(
+                                title: Text('Route for ${widget.child}')),
+                            body: Container(
+                              padding: const EdgeInsets.all(16.0),
+                              alignment: Alignment.center,
+                              child: TextField(controller: _textController),
+                            ),
+                          );
+                        },
+                      ));
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _pageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: IndexedStack(
+          index: _pageIndex,
+          children: const <Widget>[
+            NavigatorPage(child: Text('Home')),
+            NavigatorPage(child: Text('Business')),
+            NavigatorPage(child: Text('School')),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.business), title: Text('Business')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.school), title: Text('School')),
+        ],
+        currentIndex: _pageIndex,
+        onTap: (int index) {
+          setState(() {
+            _pageIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
